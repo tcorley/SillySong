6f23a4d45286479f527a3025e509e4aec20ed211
Corley, Tyler <tcorley@ebay.com>
2017-06-14 12:05:45 -0700

    let the chips fall where they may

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
+        nameField.returnKeyType = .done
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        lyricsView.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
     }
 
+    @IBAction func capitalize(_ sender: Any) {
+        nameField.text = nameField.text?.capitalized
+    }
 
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+// MARK: - Lyrics Generator
+func indexOfFirstVowel(_ name: String) -> Int {
+    let name = name.folding(options: .diacriticInsensitive, locale: .current)
+    for (index, letter) in name.characters.enumerated() {
+        if "aeiouy".characters.contains(letter) {
+            return index
+        }
+    }
+    return -1
+}
+
+
+func shortNameFromName(_ name: String) -> String {
+    if !name.isEmpty {
+        let name = name.lowercased()
+        let index = name.index(name.startIndex, offsetBy: indexOfFirstVowel(name))
+        return name.substring(from: index)
+    } else {
+        return ""
+    }
+}
+
+// join an array of strings into a single template string:
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    return lyricsTemplate.replacingOccurrences(of: "<FULL_NAME>", with: fullName).replacingOccurrences(of: "<SHORT_NAME>", with: shortNameFromName(fullName))
 }
 
